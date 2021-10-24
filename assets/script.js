89d11a8190666d3353cf03ea9b0e07194c1b9747
ksdevinney <katie@devinney.org>
2021-10-23 20:30:54 -0500

    conversion returns 2 decimal places

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ document.getElementById("output").style.visibility = "hidden";
 document.getElementById("lbs-input").addEventListener('input', function(e) {
     document.getElementById("output").style.visibility = "visible";
     let pounds = e.target.value;
-    document.getElementById("grams-output").innerHTML = pounds/0.0022046;
-    document.getElementById("kg-output").innerHTML = pounds/2.2046;
+    document.getElementById("grams-output").innerHTML = (pounds/0.0022046 * 100 / 100).toFixed(2);
+    document.getElementById("kg-output").innerHTML = (pounds/2.2046).toFixed(2);
     document.getElementById("ounces-output").innerHTML = pounds * 16;
 });
